29bf2a1e0909db942826cff931e5eef899407f58
pearl20-20 <manneshie@yahoo.com>
2023-03-09 01:50:08 -0800

    python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,28 @@
-#include "lists.h"
+include "lists.h"
+
+/**
+ * check_cycle - checks if there is a loop in the linked list
+ * @list: the head node passed to us
+ * Return: 0 if no loop, 1 if loop.
+ */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *temp, *current;
-	int counter = 0, i;
+  listint_t *fast = NULL;
+  listint_t *slow = NULL;
 
-	current = list;
-	while(current)
-	{
-		temp = list;
-		for(i = 0; i < counter; i++)
-		{
-			if (current == temp)
-				return (1);
-			temp = temp->next;
-		}
-		counter++;
-		current = current->next;
-	}
-	return (0);
+  if (!list || !list->next)
+    return (0);
+
+  slow = list;
+  fast = list->next;
+  while (fast && fast->next)
+    {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast)
+	return (1);
+    }
+
+  return (0);
 }
