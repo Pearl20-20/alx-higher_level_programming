76254846a953c60a1c5b52d6a8a48e28379b43bf
pearl20-20 <manneshie@yahoo.com>
2023-11-26 14:52:28 +0000

    Object realtional mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""adds the State object “Louisiana”
-to the database hbtn_0e_6_usa"""
+"""
+ adds the State object "Louisiana" to the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    session = Session(engine)
-    new = State(name='Louisiana')
-    session.add(new)
-    new_state = session.query(State).filter(State.name == 'Louisiana').first()
+    obj = State(name="Louisiana")
+    session.add(obj)
     session.commit()
-    print("{}".format(new_state.id))
-    session.close()
+    print(obj.id)
